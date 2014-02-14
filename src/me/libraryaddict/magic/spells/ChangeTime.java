76b64b5a167e1fcc228f27a2a624be0462145dfb
libraryaddict <libraryaddict115@yahoo.co.nz>
2014-02-14 19:00:28 +1300

    Update ChangeTime to be better

--- src/me/libraryaddict/magic/spells/ChangeTime.java
+++ src/me/libraryaddict/magic/spells/ChangeTime.java
@@ -10,7 +10,17 @@ public class ChangeTime extends Spell {
     public void invokeSpell(Player player, String[] args) {
         if (args.length > 0) {
             try {
-                int time = Integer.parseInt(args[0]);
+                String s = args[0];
+                s = s.replaceAll("\\D+", "");
+                int time = Integer.parseInt(s);
+                if (time != 0) {
+                    while (time < 1000) {
+                        time *= 10;
+                    }
+                }
+                if (args[0].toLowerCase().contains("pm")) {
+                    time += 12000;
+                }
                 player.getWorld().setTime(time);
             } catch (Exception ex) {
                 ex.printStackTrace();
